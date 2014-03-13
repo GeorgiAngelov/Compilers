45dbff3d09881cdb8b1e86a2e120f2a8505b63ec
gaatyd_112113 <gaatyd@babbage.cs.missouri.edu>
2014-03-13 08:58:48 -0500

    now we read from a file

--- HW2/pa2sdk/src/frontend.c
+++ HW2/pa2sdk/src/frontend.c
@@ -2,6 +2,19 @@
 #include "parser.h" // yyparse, yylval, token enum, etc.
 
 int main(int argc, char** argv) {
+	
+	if(argc!=2){
+		printf("To run please execute: ./pa2 <file_name>\n");
+		exit(-1);
+	}
+	
+	FILE *f = fopen(argv[1], "r");
+	if(!f){
+		printf("File could not be opened\n");
+		exit(-1);
+	}
+	//p
+	yyrestart(f);
     yyparse();
-      return 0;
+    return 0;
 }
