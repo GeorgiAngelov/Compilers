6c331e54aaf62e6100e3f145a8fd6c9553a11751
Derek Provance <aranzou@gmail.com>
2014-05-06 17:44:37 -0500

    CRAPgit add -A

--- HW4/sdk/test/decl.s
+++ HW4/sdk/test/decl.s
@@ -8,23 +8,51 @@ li $v0, 2
 sub $sp, $sp, 4
 sw $v0, 8($fp)
 sub $sp, $sp, 4
+sub $sp, $sp, 4
+sub $sp, $sp, 4
+sub $sp, $sp, 4
 li $v0, 5
 sw $v0, 0($sp)
 li $v0, 5
 lw $v1, 0($sp)
 add $sp, $sp, 4
 mul $v0, $v1, $v0
-sub $sp, $sp, 4
-sw $v0, 12($fp)
-sub $sp, $sp, 4
-lw $v0, 8($fp)
 sw $v0, 0($sp)
-lw $v0, 12($fp)
+sub $sp, $sp, 4
+sub $sp, $sp, 4
+li $v0, 10
+sw $v0, 0($sp)
+sub $sp, $sp, 4
+li $v0, 7
+sw $v0, 0($sp)
+li $v0, 4
+lw $v1, 0($sp)
+add $sp, $sp, 4
+mul $v0, $v1, $v0
+lw $v1, 0($sp)
+add $sp, $sp, 4
+mul $v0, $v1, $v0
+sw $v0, 0($sp)
+li $v0, 5
+lw $v1, 0($sp)
+add $sp, $sp, 4
+div $v0, $v1, $v0
+lw $v1, 0($sp)
+add $sp, $sp, 4
+add $v0, $v1, $v0
+sw $v0, 0($sp)
+lw $v0, 8($fp)
+lw $v1, 0($sp)
+add $sp, $sp, 4
+add $v0, $v1, $v0
+sw $v0, 0($sp)
+lw $v0, 4($fp)
 lw $v1, 0($sp)
 add $sp, $sp, 4
 add $v0, $v1, $v0
 sub $sp, $sp, 4
-sw $v0, 16($fp)
+sw $v0, 12($fp)
+sub $sp, $sp, 4
 lw $a0, 4($fp)
 li $v0, 1
 syscall
@@ -49,5 +77,6 @@ syscall
 la $a0, newline
 li $v0, 4
 syscall
+add $sp, $sp, 16
 li $v0, 10
-syscall
+ syscall
--- HW4/sdk/test/for.s
+++ HW4/sdk/test/for.s
@@ -18,13 +18,7 @@ sw $v0, 16($fp)
 lw $v0, 8($fp)
 sw $v0, 4($fp)
 _lbl0:
-sub $sp, $sp, 4
-lw $v0, 12($fp)
-sw $v0, 0($sp)
-lw $v0, 16($fp)
-lw $v1, 0($sp)
-add $sp, $sp, 4
-mul $v0, $v1, $v0
+li $v0, 10
 lw $v1, 4($fp)
 bge $v1, $v0, _lbl1
 sub $sp, $sp, 4
@@ -35,6 +29,25 @@ lw $v1, 0($sp)
 add $sp, $sp, 4
 add $v0, $v1, $v0
 sw $v0, 8($fp)
+lw $v0, 16($fp)
+sw $v0, 12($fp)
+_lbl2:
+li $v0, 15
+lw $v1, 12($fp)
+bge $v1, $v0, _lbl3
+sub $sp, $sp, 4
+lw $v0, 16($fp)
+sw $v0, 0($sp)
+li $v0, 1
+lw $v1, 0($sp)
+add $sp, $sp, 4
+add $v0, $v1, $v0
+sw $v0, 16($fp)
+lw $v0, 12($fp)
+addi $v0, $v0, 1
+sw $v0, 12($fp)
+j _lbl2
+_lbl3:
 lw $v0, 4($fp)
 addi $v0, $v0, 1
 sw $v0, 4($fp)
@@ -64,5 +77,6 @@ syscall
 la $a0, newline
 li $v0, 4
 syscall
+add $sp, $sp, 16
 li $v0, 10
-syscall
+ syscall
