e977d342f66469596b52bd2a4f43779d5b9ff94d
Michael Brush <mabcb7@mail.missouri.edu>
2014-05-14 20:29:17 -0500

    asd

--- HW4/sdk/test.s
+++ HW4/sdk/test.s
@@ -9,17 +9,17 @@ sw $v0, 4($fp)
 sub $sp, $sp, 4
 li $v0 5
 sw $v0, 8($fp)
+li $v0, 10
 add $sp, $sp, 8
 jr $ra #fred
-li $v0, 10
 		main:
 sub $sp, $sp, 4
 li $v0, 15
 sub $sp, $sp, 4
 sw $v0, 16($fp)
 li $v0 20
-sw $v0, 0($fp)
-add $sp, $sp, 16
+sw $v0, 12($fp)
+li $v0, 0
+add $sp, $sp, 12
 li $v0, 10
  syscall
-li $v0, 0
