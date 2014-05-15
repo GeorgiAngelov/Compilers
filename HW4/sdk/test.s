824785b32f6b046c0e62a51c46a3daae446727aa
gaatyd_112113 <gaatyd@babbage.cs.missouri.edu>
2014-05-14 20:11:15 -0500

    weird double assignment into global var if we declar a var and then we initialize it in main

--- HW4/sdk/test.s
+++ HW4/sdk/test.s
@@ -4,3 +4,18 @@ newline:	.asciiz "\n"
 move $fp $sp
 li $v0, 5
 sub $sp, $sp, 4
+sw $v0, 4($fp)
+		fred:
+sub $sp, $sp, 4
+li $v0 5
+sw $v0, 8($fp)
+add $sp, $sp, 8
+jr $ra #fred
+li $v0, 10
+		main:
+sub $sp, $sp, 4
+sub $sp, $sp, 4
+add $sp, $sp, 12
+li $v0, 10
+ syscall
+li $v0, 0
