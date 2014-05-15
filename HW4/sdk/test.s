fa6b0fa23d66508ae96ce1c1dd5a7cfe69e73620
Michael Brush <mabcb7@mail.missouri.edu>
2014-05-14 21:03:25 -0500

    the push

--- HW4/sdk/test.s
+++ HW4/sdk/test.s
@@ -5,21 +5,23 @@ move $fp $sp
 li $v0, 5
 sub $sp, $sp, 4
 sw $v0, 4($fp)
+li $v0, 244
+sub $sp, $sp, 4
+sw $v0, 8($fp)
 		fred:
 sub $sp, $sp, 4
-li $v0 5
-sw $v0, 8($fp)
+li $v0, 5
+sub $sp, $sp, 4
+sw $v0, 16($fp)
 li $v0, 10
-add $sp, $sp, 8
+add $sp, $sp, 12
 jr $ra #fred
 		main:
 sub $sp, $sp, 4
 li $v0, 15
 sub $sp, $sp, 4
-sw $v0, 16($fp)
-li $v0 20
-sw $v0, 12($fp)
+sw $v0, 24($fp)
 li $v0, 0
-add $sp, $sp, 12
+add $sp, $sp, 16
 li $v0, 10
  syscall
