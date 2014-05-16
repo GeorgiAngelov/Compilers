6bf9d66391346c6a9aeb6ce2e92d938a676a03a6
Michael Brush <mabcb7@mail.missouri.edu>
2014-05-15 22:19:05 -0500

    first submission

--- HW4/sdk/test.s
+++ HW4/sdk/test.s
@@ -7,16 +7,10 @@ move $fp $sp
 li $v0, 1
 sub $sp, $sp, 4
 sw $v0, 4($gp)
-li $v0, 2
-sub $sp, $sp, 4
-sw $v0, 8($gp)
 jal main_start
 		sally:
-lw $v0, 8($fp)
-# mode is sally
-sw $v0, 8($gp)
 li $v0, 10
-add $sp, $sp, 4
+add $sp, $sp, 0
 jr $ra #sally
 		fred:
 sub $sp, $sp, 12
@@ -24,15 +18,18 @@ sw $ra, -8($fp)
 sw $fp, -4($sp)
 sw $sp, 0($sp)
 move $fp, $sp
-lw $v0, 12($fp)
-sub $sp, $sp, 4
-sw $v0, 4($fp)
 jal sally
 lw $sp, 0($fp)
 lw $fp, -4($fp)
 lw $ra, -8($fp)
 add $sp, $sp, 12
-lw $v0, 16($fp)
+sub $sp, $sp, 4
+lw $v0, 4($fp)
+sw $v0, 4($sp)
+lw $v0, 8($fp)
+lw $v1, 4($sp)
+add $sp, $sp, 4
+add $v0, $v1, $v0
 add $sp, $sp, 8
 jr $ra #fred
 		main:
@@ -67,12 +64,6 @@ syscall
 la $a0, newline
 li $v0, 4
 syscall
-lw $a0, 8($gp)
-li $v0, 1
-syscall
-la $a0, newline
-li $v0, 4
-syscall
-add $sp, $sp, 8
+add $sp, $sp, 4
 li $v0, 10
  syscall
